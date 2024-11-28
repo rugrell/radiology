aaccfe4650bf3a35c02e0323ba401c3a883f9b9f
xerxez solutions <info@xerxez.in>
2024-11-28 12:46:53 +0530

    Second Commit

---
+++ webapp/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from pdfplumber import open as open_pdf
+import os
+import google.generativeai as genai
+from PIL import Image
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+
+
+
+
+
+
